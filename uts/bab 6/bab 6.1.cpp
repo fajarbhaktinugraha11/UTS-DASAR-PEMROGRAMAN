f1cf5f910cce99429d14510e1e2644ef181da372
fajarbhaktinugraha11 <57281469+fajarbhaktinugraha11@users.noreply.github.com>
2019-11-02 13:23:29 +0700

    FAJAR

---
+++ uts/bab 6/bab 6.1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+    int pencacah = 1;
+    while (pencacah <=10)
+    {
+        cout << "Teknik Informatika STIMIK Sumedang \n";
+   pencacah++;
+    }
+
+    return 0;
+
+
+}
